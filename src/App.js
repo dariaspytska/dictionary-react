4cf06319249c5352dd0da0885645589e76258f06
dariaspytska <105360871+dariaspytska@users.noreply.github.com>
2023-03-25 23:29:30 +0200

    add Dictionary.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
+      <div classNAme="container">
+        <header className="App-header text-center">DICTIONARY</header>
+        <main className="text-center">
+          <Dictionary />
+        </main>
+        <footer className="App-footer text-center">
+          <small>Coded by Daria Spytska</small>
+        </footer>
+      </div>
     </div>
   );
 }
